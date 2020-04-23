b712e2aa845ab3e8af2e49aeb1e8889aa2e76fac
James Kent <james-kent@uiowa.edu>
2020-04-22 20:58:30 -0500

    more analyses for discussion
    - add glass brain images
    - isolate the schaefer regions with the largest CNR
    - test CNR between real data and null data

--- nibsAnalysis/notebook_functions.py
+++ nibsAnalysis/notebook_functions.py
@@ -3,12 +3,17 @@ functions to help with keeping the notebook uncluttered
 (and allow testing of the functions)
 """
 import re
+from multiprocessing.pool import Pool
+
 import seaborn as sns
 import pandas as pd
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
-from multiprocessing.pool import Pool
+from matplotlib.colors import ListedColormap
+import matplotlib as mpl
+from nilearn.plotting import plot_connectome
+
 
 from rpy2 import robjects as robj
 from rpy2.robjects import pandas2ri
@@ -337,8 +342,36 @@ def return_lower_matrix(adj_matrix):
     return from_1d_to_2d_lower_matrix(lower_tri, adj_matrix.columns)
 
 
-# def make_glass_brain(p_value_matrix1, matrix1_label, p_value_matrix2, matrix2_label, cmap_dict,
-#                     coords_df):
+def make_glass_brain(overlap_df, coords_df):
+    coords = coords_df[["X", "Y", "Z"]].values
+    coord_names = coords_df["Cluster ID"].values
+    
+    cmap_list = sns.xkcd_palette(["royal blue", "olive green", "bright yellow"])
+    cmap=ListedColormap(cmap_list)
+    
+    fig, axes = plt.subplots(ncols=2, figsize=(14,5), gridspec_kw={'width_ratios': [23/24, 1/24]})
+    
+    plot_connectome(overlap_df, axes=axes[0], figure=fig, edge_cmap=cmap, node_coords=coords,
+                edge_vmin=0.9,
+                edge_vmax=3,
+                node_size=200,
+                edge_threshold=0,
+                colorbar=False)
+
+    # make the color bar
+    colorbar = mpl.colorbar.ColorbarBase(axes[1], cmap=cmap, orientation='vertical', boundaries=[0, 1, 2, 3])
+    r = 3.0 
+    colorbar.set_ticks([0.0 + r / 3 * (0.5 + i) for i in range(3)])
+    colorbar.set_ticklabels(["LSA", "LSS", "Both"])
+    colorbar.ax.tick_params(labelsize=20)
+
+    # label the nodes with their number
+    for ax in fig.axes[2:]:
+        for name, node in zip(coord_names, ax.collections[0].get_offsets()):
+            ax.annotate(name, node, ha='center', va='center', zorder=5000)
+    
+    return fig
+
     
 def make_comparison_matrix(p_value_matrix1, matrix1_label, p_value_matrix2, matrix2_label, cmap_dict,
                            rois='schaefer'):
